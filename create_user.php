73b7ce35e93d7c15fac9cb03af4af59d41295f51
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2016-06-22 12:15:06 +0000

    Update create_user.php fixture script

--- create_user.php
+++ create_user.php
@@ -1,5 +1,5 @@
 <?php
-include 'rb.php';
+include 'lib/RedBeanPHP.php';
 
 R::setup( 'mysql:host=127.0.0.1;dbname=c9',
         'alessandro_bugat', '' );
@@ -25,6 +25,16 @@ $user->token = '';
 $user->role_id = '2';
 $id = R::store( $user );
 
+$user = R::dispense( 'user' );
+$user->name = 'Alessandro';
+$user->surname = 'Bugatti';
+$user->username = 'admin';
+$user->password = '$2y$10$d/doEy2cRSCfIaIaoQ4QAOWdT13SzvXdVZW1M4xQxHwa.Xpk5ZygS'; //pippo
+$user->token = '';
+$user->role_id = '3';
+$id = R::store( $user );
+
+
 $role = R::dispense( 'role' );
 $role->description = 'teacher';
 $id = R::store( $role );
@@ -32,3 +42,8 @@ $id = R::store( $role );
 $role = R::dispense( 'role' );
 $role->description = 'student';
 $id = R::store( $role );
+
+$role = R::dispense( 'role' );
+$role->description = 'admin';
+$id = R::store( $role );
+
