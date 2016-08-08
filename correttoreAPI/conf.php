b7f13060e360379e6c4256dfa777e876a278a322
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2016-08-08 14:04:29 +0000

    Add task path in filesystem

--- correttoreAPI/conf.php
+++ correttoreAPI/conf.php
@@ -3,3 +3,4 @@ $app['redbean.database'] = 'mysql:host=127.0.0.1;dbname=c9';
 $app['redbean.username'] = 'alessandro_bugat';
 $app['redbean.password'] = '';
 $app['version'] = 1;
+$app['task.path'] = '../tasks/';
