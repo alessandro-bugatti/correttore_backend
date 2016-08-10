ac04d885d75fdaaa800b926898953e19a4d0a11b
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2016-08-10 09:55:53 +0000

    Add static function to remove directory

--- src/Correttore/Util/Utility.php
+++ src/Correttore/Util/Utility.php
@@ -18,4 +18,24 @@ class Utility{
         //Maybe there could be some sort of file type checking..
         $file->move($app['task.path'] . '/' . $folder, $name.'.'.$type);
     }
+    
+    /**
+     * Remove a file or a directory even if it contains files or subdir
+     */
+    private static function removeDir($target) {
+        if(is_dir($target)){
+            $files = glob( $target . '/*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
+            foreach( $files as $file )
+                self::removeDir( $file );      
+            rmdir( $target );
+        } 
+        elseif (is_file($target)) {
+            unlink( $target );  
+        }
+    }
+    
+    public static function rmTaskFolder(Application $app, $folder)
+    {
+        self::removeDir($app['task.path'] . $folder);
+    }
 }
