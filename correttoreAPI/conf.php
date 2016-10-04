3d471cea018292991a4ee70f8806c02ce9628c68
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2016-10-04 13:25:54 +0000

    Clean up

--- correttoreAPI/conf.php
+++ correttoreAPI/conf.php
@@ -1,8 +1,22 @@
 <?php
 // Add the configuration, etc. here
+
+//Enable or disable debug
 $app['debug'] = true;
+
+//Database configuration for RedBean
 $app['redbean.database'] = 'mysql:host=127.0.0.1;dbname=c9';
 $app['redbean.username'] = 'alessandro_bugat';
 $app['redbean.password'] = '';
+
+//API version
 $app['version'] = 1;
+
+//Folder which contains the tasks
 $app['task.path'] = '../tasks/';
+
+//C++ driver executable
+$app['cppdriver'] = '../driver';
+
+//Folder to store some kind of problem during evaluation phase
+$app['evaluation.dir'] = '../temp';
