1d706b844dd149c71e236225035e2e781885eec5
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2016-10-13 08:44:46 +0000

    Add solution creation

--- create_task.php
+++ create_task.php
@@ -4,10 +4,19 @@ include 'lib/RedBeanPHP.php';
 R::setup( 'mysql:host=127.0.0.1;dbname=c9',
         'alessandro_bugat', '' );
 
-R::exec('drop table task');
+R::exec('drop table task_test');
 R::exec('drop table category');
+R::exec('drop table task');
+
 //R::wipe('task');
 //R::wipe('category');
+
+$test = R::dispense('test');
+$test->description = "Test di prova";
+$test->is_on = '1';
+$test->creation_date = '2016-04-10';
+
+
 
 $task = R::dispense( 'task' );
 
@@ -21,7 +30,7 @@ $task->category_id = '1';
 $task->user_id = '1';
 
 
-
+$test->sharedTaskList[] = $task;
 
 $id = R::store( $task );
 
@@ -38,9 +47,21 @@ $task->user_id = '1';
 
 $id = R::store( $task );
 
+$test->sharedTaskList[] = $task;
+
 R::exec('ALTER TABLE task ADD UNIQUE(title)');
 R::exec('ALTER TABLE task ADD UNIQUE(short_title)');
+ 
+$id = R::store($test);
 
+$solution = R::dispense('solution');
+$solution->score = 8;
+$solution->file='somma.cpp';
+$solution->submitted='2016-10-04 23:22:00';
+$solution->task_id = 1;
+$solution->user_id = 1;
+
+$id = R::store($solution);
 
 $category = R::dispense( 'category' );
 $category->description = 'Sequenza';
@@ -61,4 +82,6 @@ $category->type = 'Programmazione';
 
 
 $id = R::store( $category );
+
+
 
