b56e199a21351359bd37875d2eeae4ceda899683
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2017-11-22 18:27:33 +0100

    Add batch students creation

---
+++ tools/create_users_batch.php
@@ -0,0 +1,37 @@
+<?php
+
+$group = readline("Inserisci il nome del gruppo: ");
+$n_of_users = readline("Quanti utenti vuoi creare?");
+
+$command_file = fopen($group . "_creation.php",'w');
+$password_file = fopen($group . "_password.txt",'w');
+//Create header
+fwrite($command_file,"<?php \n\ninclude '../init/conf.php';");
+fwrite($password_file,"File delle password");
+
+//Create group
+
+fwrite($command_file,"\n\$groupset = R::dispense( 'groupset' );");
+fwrite($command_file,"\n\$groupset->description = '$group';");
+fwrite($command_file,"\n\$id = R::store( \$groupset);");
+
+
+//Create users
+
+for ($i = 1; $i <= $n_of_users; $i++){
+    $p = bin2hex(openssl_random_pseudo_bytes(4));
+    $user = $group . "_user" . $i;
+    fwrite($password_file,"\n" . $user . ";" . $p );
+    
+    fwrite($command_file,"\n\\\\User: $user");
+    fwrite($command_file,"\n\$user = R::dispense( 'user' );");
+    fwrite($command_file,"\n\$user->name = '$user';");
+    fwrite($command_file,"\n\$user->surname = '$user';");
+    fwrite($command_file,"\n\$user->username = '$user';");
+    fwrite($command_file,"\n\$user->password = '" . password_hash($p, PASSWORD_DEFAULT) ."'");
+    fwrite($command_file,"\n\$user->token = '';");
+    fwrite($command_file,"\n\$user->role_id = '2';");
+    fwrite($command_file,"\n\$user->sharedGroupsetList[] = \$groupset;");
+    fwrite($command_file,"\n\$id = R::store( \$user );\n");
+}
+
