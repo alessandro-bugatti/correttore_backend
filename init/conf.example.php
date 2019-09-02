5f4f41fac4578e75bc63831c3f3e74eeac50f554
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2019-09-02 21:43:03 +0200

    Update parameters

--- init/conf.example.php
+++ init/conf.example.php
@@ -2,5 +2,5 @@
 
 include '../lib/RedBeanPHP.php';
 
-R::setup( 'mysql:host=127.0.0.1;dbname=c9',
-        'alessandro_bugat', '' );
+R::setup( 'mysql:host=127.0.0.1;dbname=correttore',
+        'correttore', 'correttore' );
