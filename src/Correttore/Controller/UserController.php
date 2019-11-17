2dc1189cfdedfdd6548de838f0dc91614ad30de3
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-11-17 09:43:11 +0100

    Fix

--- src/Correttore/Controller/UserController.php
+++ src/Correttore/Controller/UserController.php
@@ -141,7 +141,7 @@ class UserController{
             return new Response('', 404);
         if ($user->role->description != 'student')
             return new JsonResponse(["error" => "Wrong role", 
-                "role" => $request->request->get("role") ], 403);
+                "role" => $user->role->description ], 403);
         //TODO: Check if the student belongs to this teacher
         //Now it is not possible because there isn't any relation
         //between a teacher and a user
